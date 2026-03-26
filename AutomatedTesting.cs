916f9b49d208a12171727a21570dfce4a3c85cba
NorLaidAn <nedger.five@gmail.com>
2026-03-27 00:36:20 +0200

    Add EHU search test

--- AutomatedTesting.cs
+++ AutomatedTesting.cs
@@ -13,21 +13,49 @@ namespace AT_Work1
         public void EHUSiteWorkTest()
         {
             IWebDriver driver = new ChromeDriver();
+            Actions actions = new Actions(driver);
             driver.Manage().Window.Maximize();
-
             driver.Navigate().GoToUrl("https://en.ehu.lt");
+
             IWebElement aboutTab = driver.FindElement(By.XPath("//a[text()='About']"));
 
-            Actions actions = new Actions(driver);
-            actions.MoveToElement(aboutTab).Perform();
             
+            actions.MoveToElement(aboutTab).Perform();
+
             aboutTab.Click();
 
             string currentUrl = driver.Url;
             Assert.That(currentUrl, Is.EqualTo("https://en.ehuniversity.lt/about/"));
 
             driver.FindElement(By.XPath("//title[contains(text(), 'About')]"));
+
+            driver.Quit();
+        }
+
+        public void EHUSearchTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            Actions actions = new Actions(driver);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://en.ehu.lt");
+
+            IWebElement search = driver.FindElement(By.XPath("//div[@class='header-search']"));
             
+            actions.MoveToElement(search).Perform();
+
+            IWebElement searchBox = driver.FindElement(By.XPath("//input[@class='form-control']"));
+
+            searchBox.SendKeys("study programs");
+
+            IWebElement submitButton = driver.FindElement(By.XPath("//button[@class='btn btn-info']"));
+
+            submitButton.Click();
+
+            string currentUrl = driver.Url;
+            Assert.That(currentUrl, Is.EqualTo("https://en.ehuniversity.lt/?s=study+programs"));
+
+            driver.FindElement(By.XPath("//div[@class='content search-results']"));
+
             driver.Quit();
         }
     }
