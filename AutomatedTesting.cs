887c54eb3f4cabddf669e532f8ae836537a2d526
NorLaidAn <nedger.five@gmail.com>
2026-03-26 23:25:33 +0200

    Add test for About EHU page

--- AutomatedTesting.cs
+++ AutomatedTesting.cs
@@ -1,7 +1,34 @@
-﻿namespace AT_Work1
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace AT_Work1
 {
+    [TestFixture]
     public class AutomatedTesting
     {
+        [Test]
+        public void EHUSiteWorkTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
 
+            driver.Navigate().GoToUrl("https://en.ehu.lt");
+            IWebElement aboutTab = driver.FindElement(By.XPath("//a[text()='About']"));
+
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(aboutTab).Perform();
+            
+            aboutTab.Click();
+
+            string currentUrl = driver.Url;
+            Assert.That(currentUrl, Is.EqualTo("https://en.ehuniversity.lt/about/"));
+
+            driver.FindElement(By.XPath("//title[contains(text(), 'About')]"));
+            
+            driver.Quit();
+        }
     }
 }
