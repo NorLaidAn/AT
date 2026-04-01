f9d8561f5b222e57a5467a72232bdee06cd66f6e
NorLaidAn <nedger.five@gmail.com>
2026-04-01 17:58:29 +0300

    Add AT-2

---
+++ NUnit-AT-2/NUnitTests.cs
@@ -0,0 +1,101 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+[assembly: Parallelizable(ParallelScope.All)]
+
+namespace NUnit_AT_2
+{
+    [TestFixture]
+    [Category("UI testing")]
+    public class NUnitTests
+    {
+        private ThreadLocal<IWebDriver> driver = new ThreadLocal<IWebDriver>();
+        private ThreadLocal<Actions> actions = new ThreadLocal<Actions>();
+
+        [SetUp]
+        public void Setup()
+        {
+            driver.Value = new ChromeDriver();
+            actions.Value = new Actions(driver.Value);
+            driver.Value.Manage().Window.Maximize();
+            driver.Value.Navigate().GoToUrl("https://en.ehu.lt");
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Value.Quit();
+        }
+
+        [Test]
+        [Category("WorkCheck")]
+        [TestCase("https://en.ehuniversity.lt/about/")]
+        public void EHUSiteWorkTest(string url)
+        {
+            IWebElement aboutTab = driver.Value.FindElement(By.XPath("//a[text()='About']"));
+
+            actions.Value.MoveToElement(aboutTab).Perform();
+
+            aboutTab.Click();
+
+            string currentUrl = driver.Value.Url;
+            Assert.That(currentUrl, Is.EqualTo(url));
+
+            driver.Value.FindElement(By.XPath("//title[contains(text(), 'About')]"));
+        }
+
+        [Test]
+        [TestCase("https://en.ehuniversity.lt/?s=study+programs")]
+        public void EHUSearchTest(string url)
+        {
+            IWebElement search = driver.Value.FindElement(By.XPath("//div[@class='header-search']"));
+
+            actions.Value.MoveToElement(search).Perform();
+
+            IWebElement searchBox = driver.Value.FindElement(By.XPath("//input[@class='form-control']"));
+
+            searchBox.SendKeys("study programs");
+
+            IWebElement submitButton = driver.Value.FindElement(By.XPath("//button[@class='btn btn-info']"));
+
+            submitButton.Click();
+
+            string currentUrl = driver.Value.Url;
+            Assert.That(currentUrl, Is.EqualTo(url));
+
+            driver.Value.FindElement(By.XPath("//div[@class='content search-results']"));
+        }
+
+        [Test]
+        [Category("WorkCheck")]
+        public void EHULanguageTest()
+        {
+            IWebElement languageChanger = driver.Value.FindElement(By.XPath("//li[a[text()='en']]"));
+
+            actions.Value.MoveToElement(languageChanger).Perform();
+
+            IWebElement changeButton = driver.Value.FindElement(By.XPath("//a[text()='lt']"));
+            changeButton.Click();
+
+            string currentUrl = driver.Value.Url;
+            Assert.That(currentUrl, Does.Contain("lt."));
+            driver.Value.FindElement(By.XPath("//a[text()='Apie mus']"));
+        }
+
+        [Test]
+        [Category("WorkCheck")]
+        public void EHUContactFormTest()
+        {
+            IWebElement contactsHref = driver.Value.FindElement(By.XPath("//li[contains(@class,'menu-item-17512')]//a[contains(@href,'contacts')]"));
+            contactsHref.Click();
+
+            Assert.That(driver.Value.FindElement(By.XPath("//em[text()='consult@ehu.lt']")).Displayed);
+
+            Assert.That(driver.Value.FindElement(By.XPath("//td[contains(., '+370')]")).Displayed);
+
+            Assert.That(driver.Value.FindElement(By.XPath("//a[contains(@href, 'facebook') and contains(text(), 'University')]")).Displayed);
+        }
+    }
+}
---
+++ xUnit-AT-2/xUnitTests.cs
@@ -0,0 +1,120 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using Xunit;
+
+[assembly: CollectionBehavior(MaxParallelThreads = 4)]
+
+namespace xUnit_AT_Parallel
+{
+    public class TestsXUnit
+    {
+        public ChromeDriver Setup()
+        {
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://en.ehu.lt");
+            return driver;
+        }
+
+        public void TearDown(ChromeDriver driver)
+        {
+            driver.Quit();
+        }
+    }
+    public class A()
+    {
+        [Theory]
+        [InlineData("https://en.ehuniversity.lt/about/")]
+        public void EHUSiteWorkTest(string url)
+        {
+            TestsXUnit test = new TestsXUnit();
+            var driver = test.Setup();
+            var actions = new Actions(driver);
+
+            IWebElement aboutTab = driver.FindElement(By.XPath("//a[text()='About']"));
+            actions.MoveToElement(aboutTab).Perform();
+            aboutTab.Click();
+
+            string currentUrl = driver.Url;
+            Assert.Equal(url, currentUrl);
+
+            driver.FindElement(By.XPath("//title[contains(text(), 'About')]"));
+
+            test.TearDown(driver);
+        }
+    }
+
+    public class B()
+    {
+        [Theory]
+        [InlineData("https://en.ehuniversity.lt/?s=study+programs")]
+        public void EHUSearchTest(string url)
+        {
+            TestsXUnit test = new TestsXUnit();
+            var driver = test.Setup();
+            var actions = new Actions(driver);
+
+            IWebElement search = driver.FindElement(By.XPath("//div[@class='header-search']"));
+            actions.MoveToElement(search).Perform();
+
+            IWebElement searchBox = driver.FindElement(By.XPath("//input[@class='form-control']"));
+            searchBox.SendKeys("study programs");
+
+            IWebElement submitButton = driver.FindElement(By.XPath("//button[@class='btn btn-info']"));
+            submitButton.Click();
+
+            string currentUrl = driver.Url;
+            Assert.Equal(url, currentUrl);
+
+            driver.FindElement(By.XPath("//div[@class='content search-results']"));
+
+            test.TearDown(driver);
+        }
+    }
+
+    public class C()
+    {
+        [Fact]
+        public void EHULanguageTest()
+        {
+            TestsXUnit test = new TestsXUnit();
+            var driver = test.Setup();
+            var actions = new Actions(driver);
+
+            IWebElement languageChanger = driver.FindElement(By.XPath("//li[a[text()='en']]"));
+            actions.MoveToElement(languageChanger).Perform();
+
+            IWebElement changeButton = driver.FindElement(By.XPath("//a[text()='lt']"));
+            changeButton.Click();
+
+            string currentUrl = driver.Url;
+            Assert.Contains("lt", currentUrl);
+
+            driver.FindElement(By.XPath("//a[text()='Apie mus']"));
+
+            test.TearDown(driver);
+        }
+    }
+
+    public class D()
+    {
+        [Fact]
+        public void EHUContactFormTest()
+        {
+            TestsXUnit test = new TestsXUnit();
+            var driver = test.Setup();
+            var actions = new Actions(driver);
+
+            IWebElement contactsHref = driver.FindElement(By.XPath("//li[contains(@class,'menu-item-17512')]//a[contains(@href,'contacts')]"));
+            contactsHref.Click();
+
+            Assert.True(driver.FindElement(By.XPath("//em[text()='consult@ehu.lt']")).Displayed);
+            Assert.True(driver.FindElement(By.XPath("//td[contains(., '+370')]")).Displayed);
+            Assert.True(driver.FindElement(By.XPath("//a[contains(@href, 'facebook') and contains(text(), 'University')]")).Displayed);
+
+            test.TearDown(driver);
+        }
+    }    
+}
