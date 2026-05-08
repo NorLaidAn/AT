1313a40bab0c58dda8b7d05079f2ed91480e8c40
NorLaidAn <nedger.five@gmail.com>
2026-05-08 15:29:47 +0300

    Add Work5

---
+++ AT-Work5/EHUHomePage.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AT_Work4
+{
+    internal class EHUHomePage
+    {
+        private IWebDriver driver;
+        private Actions actions;
+
+        public IWebElement aboutTab => driver.FindElement(By.XPath("//a[text()='About']"));
+        public IWebElement search => driver.FindElement(By.XPath("//div[@class='header-search']"));
+        public IWebElement searchBox => driver.FindElement(By.XPath("//input[@class='form-control']"));
+        public IWebElement submitButton => driver.FindElement(By.XPath("//button[@class='btn btn-info']"));
+        public IWebElement languageChanger => driver.FindElement(By.XPath("//li[a[text()='en']]"));
+        public IWebElement contactsHref => driver.FindElement(By.XPath("//li[contains(@class,'menu-item-17512')]//a[contains(@href,'contacts')]"));
+
+        public EHUHomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            this.actions = new Actions(driver);
+        }
+        public void HoverAndCLick(IWebElement element)
+        {
+            actions.MoveToElement(element).Perform();
+            element.Click();
+        }
+        public void Search(string str)
+        {
+            actions.MoveToElement(search).Perform();
+            searchBox.Clear();
+            searchBox.SendKeys(str);
+            submitButton.Click();
+        }
+        public void ChangeLanguage(string str)
+        {
+            actions.MoveToElement(languageChanger).Perform();
+            IWebElement changeButton = driver.FindElement(By.XPath($"//a[text()='{str}']"));
+            changeButton.Click();
+        }
+        public string GetCurrentUrl() => driver.Url;
+        public string GetPageTitle() => driver.Title;
+    }
+}
---
+++ AT-Work5/EHUSteps.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using Reqnroll;
+
+namespace AT_Work4
+{
+    [Binding]
+    public class EHUSteps
+    {
+        private IWebDriver driver;
+        private EHUHomePage page;
+
+        [BeforeScenario]
+        public void Setup()
+        {
+            driver = DriverSingleton.Get();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://en.ehu.lt");
+
+            page = new EHUHomePage(driver);
+        }
+
+        [AfterScenario]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+
+        [Given("user is on EHU homepage")]
+        public void GivenUserIsOnHomepage()
+        {
+            // alredy opend
+        }
+
+        [When("user clicks About tab")]
+        public void WhenUserClicksAbout()
+        {
+            page.HoverAndCLick(page.aboutTab);
+        }
+
+        [Then("About page should be opened")]
+        public void ThenAboutPageOpened()
+        {
+            Assert.That(page.GetCurrentUrl(), Does.Contain("about"));
+        }
+
+        [When("user searches for {string}")]
+        public void WhenUserSearches(string query)
+        {
+            page.Search(query);
+        }
+
+        [Then("search results page should be opened")]
+        public void ThenSearchResults()
+        {
+            Assert.That(page.GetCurrentUrl(), Does.Contain("?s="));
+        }
+
+        [When("user changes language to {string}")]
+        public void WhenChangeLanguage(string lang)
+        {
+            page.ChangeLanguage(lang);
+        }
+
+        [Then("Lithuanian version should be displayed")]
+        public void ThenLanguageChanged()
+        {
+            Assert.That(page.GetCurrentUrl(), Does.Contain("lt."));
+        }
+
+        [When("user opens contacts page")]
+        public void WhenOpenContacts()
+        {
+            page.contactsHref.Click();
+        }
+
+        [Then("contact information should be visible")]
+        public void ThenContactsVisible()
+        {
+            Assert.That(driver.FindElement(By.XPath("//em[text()='consult@ehu.lt']")).Displayed);
+        }
+    }
+}
